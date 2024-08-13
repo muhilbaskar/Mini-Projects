6aeffea2177004be5d2dfae4e2527045abe7f0a6
Muhilselvam Baskar <149035190+muhilbaskar@users.noreply.github.com>
2024-08-13 20:20:03 +0530

    Add files via upload

---
+++ Counter Program/script.js
@@ -0,0 +1,21 @@
+const increasebtn = document.getElementById("increasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const decreasebtn = document.getElementById("decreasebtn");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+increasebtn.onclick = function(){
+    count ++;
+    countLabel.textContent = count;
+};
+
+resetbtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+};
+
+decreasebtn.onclick = function(){
+    count --;
+    countLabel.textContent = count;
+};
